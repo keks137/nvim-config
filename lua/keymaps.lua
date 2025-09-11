2d087b6adbdd54d9407bef727c13be4405dd6534
Jonas Guntz <guntzjonas@gmail.com>
2025-09-11 14:58:25 +0200

    idk

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,17 +1,15 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>o', '<cmd>only<CR>', { desc = 'Make [O]nly Window' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>l', vim.lsp.buf.code_action, { desc = 'Open diagnostic Qickfix So[l]ution' }) -- great letter, I know
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>o", "<cmd>only<CR>", { desc = "Make [O]nly Window" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>n", vim.lsp.buf.code_action, { desc = "Open diagnostic Qickfix Solutio[n]" }) -- great letter, I know
 
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
-vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'Filetree [E]xplorer' })
-vim.keymap.set('n', '<leader>e', '<cmd>Oil<CR>', { desc = 'Filetree [E]xplorer' })
-vim.keymap.set('n', '<leader>Et', '<cmd>term<CR>', { desc = '[T]erminal' })
-vim.keymap.set('n', '<leader>Er', ':update <CR>:source ~/.config/nvim/init.lua<CR>',  { desc = '[R]eload config' })
-vim.keymap.set('n', '<leader>Et', '<cmd>term<CR>', { desc = '[T]erminal' })
+vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Filetree [E]xplorer" })
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Filetree [E]xplorer" })
+vim.keymap.set("n", "<leader>Et", "<cmd>term<CR>", { desc = "[T]erminal" })
+vim.keymap.set("n", "<leader>Er", ":update <CR>:source ~/.config/nvim/init.lua<CR>", { desc = "[R]eload config" })
+vim.keymap.set("n", "<leader>Et", "<cmd>term<CR>", { desc = "[T]erminal" })
 
 -- emergency format
-vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, { desc = '[F]ormat' })
-
+vim.keymap.set("n", "<leader>F", vim.lsp.buf.format, { desc = "[F]ormat" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,5 +2,5 @@ return {
 	"NMAC427/guess-indent.nvim",
 	"vimwiki/vimwiki",
 
-	{ import = "plugins" },
+	{ import = "plugins" }, -- adds plugins/*.lua
 }
